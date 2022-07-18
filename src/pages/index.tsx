ce2cba2ab6b1568c4b4aaa8f044454776d6ab6a5
lee unseo <lee.unseo17@gmail.com>
2022-07-18 23:51:05 +0900

    itroduce grommet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,39 @@
-import * as React from "react"
-// import type { GetServerDataProps, GetServerDataReturn } from "gatsby"
+import * as React from "react";
+import { Grommet, Box, Header, Footer, Text, Button } from 'grommet';
+import { Up } from 'grommet-icons';
 
-const Home = () => <div>Hello World!</div>
-export default Home
+const expand = 'sm'
+
+const MariHeader = () => (
+  <Header background="brand">
+    <Box align="center">
+      <Text>marimari</Text>
+    </Box>
+  </Header> 
+);
+
+const MainPage = () => (
+  <>
+    <MariHeader></MariHeader>
+    <MariFooter></MariFooter>
+  </>
+);
+
+const MariFooter = () => (
+ <Footer background="brand">
+    <Text>marimari</Text>
+    <Box align="right">
+      <Button icon={<Up />}>
+      </Button>
+    </Box>
+  </Footer> 
+);
+
+const Home = () => (
+  <Grommet plain>
+    <div>
+      <MainPage></MainPage>
+    </div>
+  </Grommet>
+);
+export default Home;
