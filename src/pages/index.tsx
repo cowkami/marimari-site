3452586b791b9ae2cd7d8c19d07c184ece8383ff
lee unseo <lee.unseo17@gmail.com>
2022-07-18 16:45:59 +0900

    test deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,5 @@
 import * as React from "react"
-import type { GetServerDataProps, GetServerDataReturn } from "gatsby"
+// import type { GetServerDataProps, GetServerDataReturn } from "gatsby"
 
-type ServerDataProps = {
-  hello: string
-}
-
-
-const Page = () => <div>Hello World</div>
-export default Page
-
-export async function getServerData(
-  props: GetServerDataProps
-): GetServerDataReturn<ServerDataProps> {
-  return {
-    status: 200,
-    headers: {},
-    props: {
-      hello: "world",
-    },
-  }
-}
+const Home = () => <div>Hello World!</div>
+export default Home
