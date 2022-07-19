3c36ac5b4880de157d9b2c7472e4914509f8de50
lee unseo <lee.unseo17@gmail.com>
2022-07-20 00:19:02 +0900

    implement header and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,101 @@
 import * as React from "react";
-import { Grommet, Box, Header, Footer, Text, Button } from 'grommet';
-import { Up } from 'grommet-icons';
+import { Grommet, Box, Text, Button } from 'grommet';
+import { Up, Phone, Mail } from "grommet-icons";
+import { IncomingMessage } from "http";
+import { traceDeprecation } from "process";
 
 const expand = 'sm'
 
+const Marimari = (
+  <>
+    marimari &trade;
+  </>
+)
+
+const mariTheme = {
+  global: {
+    font: {
+      family: "Roboto"
+    },
+    colors: {
+      brand: "#E3C576",
+      assistance: "#2F4F4F",
+      accent: "#FFFAF1"
+    }
+  }
+}
+
 const MariHeader = () => (
-  <Header background="brand">
-    <Box align="center">
-      <Text>marimari</Text>
+  <Box
+    tag="header"
+    direction="row"
+    background="brand"
+    pad={{ vertical: "medium", horizontal: "medium"}}
+    justify="center"
+  >
+    <Text weight="bold" color="assistance"> { Marimari } </Text>
+  </Box> 
+);
+
+const MariFooter = () => (
+  <Box
+    tag="footer"
+    direction="column"
+    background="assistance"
+    pad={{ vertical: "medium", horizontal: "medium"}}
+    justify="center"
+  >
+    <Box direction="row" justify="center">
+      <Text weight="bold" color="accent">marimari &trade;</Text>
     </Box>
-  </Header> 
+    <Box direction="row" justify="center">
+      <Phone />
+      <Text> {"0120-345-6789"} </Text>
+    </Box>
+    <Box direction="row" justify="center">
+      <Mail />
+      <Text>
+        {"mari_mari_croffle@gmail.com"}
+      </Text>
+    </Box>
+  </Box> 
 );
 
 const MainPage = () => (
   <>
-    <MariHeader></MariHeader>
-    <MariFooter></MariFooter>
+    <MariHeader />
+    <MariFooter />
   </>
 );
 
-const MariFooter = () => (
- <Footer background="brand">
-    <Text>marimari</Text>
-    <Box align="right">
-      <Button icon={<Up />}>
-      </Button>
+const NavBar = () => (
+  <Box
+    tag="footer"
+    direction="row"
+    align="center"
+    background="accent"
+    justify="between"
+    pad={{ vertical: "small", horizontal: "medium"}}
+  >
+    <Box align="left">
+      <Text weight="bold">marimari</Text>
     </Box>
-  </Footer> 
+    <Box align="right">
+      <Button
+        icon={<Up />}
+        onClick={() => {}}
+      />
+    </Box>
+  </Box> 
 );
 
 const Home = () => (
-  <Grommet plain>
-    <div>
-      <MainPage></MainPage>
-    </div>
+  <Grommet
+    theme={mariTheme}
+    full={true}
+  >
+    <MainPage />
+    <NavBar />
   </Grommet>
 );
 export default Home;
